95e1d7a49fd724c1bf994a6f20dbf7f9e9a49453
Pior Bastida <pior@pbastida.net>
2018-04-10 22:56:06 -0400

    Use setuptools rather than distutils2 for packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,35 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    setup_requires=['d2to1'],
-    d2to1=True,
+with open('README.rst') as fd:
+    description = fd.read()
+
+VERSION = '1.1.1.dev0'
+
+setup(
+    name='pyramid_google_login',
+    version=VERSION,
+    description='Google Login for Pyramid',
+    long_description=description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+    ],
+    keywords=['google', 'login', 'wsgi', 'pyramid', 'auth', 'authentication'],
+    author='Pior Bastida',
+    author_email='pior@pbastida.net',
+    url='https://github.com/ludia/pyramid_google_login',
+    license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
+
+    packages=find_packages(),
+    package_data={'pyramid_google_login': ['static/*.*', 'templates/*.*']},
+
+    install_requires=['pyramid_mako', 'requests', 'six'],
 )
