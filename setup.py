43d66c13586673903b04c0e8c3fcd940439b15bc
Pior Bastida <pior@pbastida.net>
2018-04-12 20:36:00 -0400

    Release v1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as fd:
     description = fd.read()
 
-VERSION = '1.1.1.dev0'
+VERSION = '1.2.0'  # maintained by release tool
 
 setup(
     name='pyramid_google_login',
